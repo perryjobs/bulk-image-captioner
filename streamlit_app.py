f2dab4ff9bb03710859e4c6b8f92defdfd5a8765
perryjobs <perry.jobs101@gmail.com>
2025-07-01 22:38:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,7 +153,7 @@ if file and uploaded_images:
                     y_cursor += line_heights[i] + line_spacing
 
                 if show_previews:
-    st.image(img, caption=f"{img_name}{suffix}", use_column_width=True)
+                    st.image(img, caption=f"{img_name}{suffix}", use_column_width=True)
 
                 if enable_download:
                     outname = os.path.splitext(img_name)[0] + f"{suffix}.png"
